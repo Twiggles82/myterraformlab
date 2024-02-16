e587ba59ac96e352be695404b681e7610502a5b5
Simon Greenhalgh <simon.greenhalgh@dxc.com>
2024-02-16 13:29:20 +0000

    working setup

--- computea.tf
+++ computea.tf
@@ -198,7 +198,7 @@ resource "aws_instance" "myUbuntu2" {
      }
  }
 
-resource "local_file" "ans-inventory" {
+resource "local_file" "tf-hosts" {
   filename = "C:\\repos\\ownrepo\\myterraformlab\\hosts"
   content  = <<EOF
 [ad-root]
@@ -232,8 +232,65 @@ EOF
 }
 
 
-resource "local_file" "excel-inv" {
+resource "local_file" "ans-hosts" {
+  filename = "C:\\repos\\ownrepo\\myansiblelab\\hosts"
+  content  = <<EOF
+[ad-root]
+${aws_instance.ad-root.private_ip}
+
+[ad-comp]
+${aws_instance.ad-comp.private_ip}
+
+[ad-user]
+${aws_instance.ad-user.private_ip}
+
+[ca-comp]
+${aws_instance.ca-comp.private_ip}
+
+[ca-user]
+${aws_instance.ca-user.private_ip}
+
+[ocsp-comp]
+${aws_instance.ocsp-comp.private_ip}
+
+[ocsp-user]
+${aws_instance.ocsp-user.private_ip}
+
+[comp-desktop]
+${aws_instance.comp-desktop.private_ip}
+
+[user-desktop]
+${aws_instance.user-desktop.private_ip}
+
+EOF
+}
+
+resource "local_file" "tf-inv-csv" {
   filename = "C:\\repos\\ownrepo\\myterraformlab\\excel-inv.csv"
+  content  = <<EOF
+ad-root,${aws_instance.ad-root.private_ip}
+
+ad-comp,${aws_instance.ad-comp.private_ip}
+
+ad-user,${aws_instance.ad-user.private_ip}
+
+ca-comp,${aws_instance.ca-comp.private_ip}
+
+ca-user,${aws_instance.ca-user.private_ip}
+
+ocsp-comp,${aws_instance.ocsp-comp.private_ip}
+
+ocsp-user,${aws_instance.ocsp-user.private_ip}
+
+comp-desktop,${aws_instance.comp-desktop.private_ip}
+
+user-desktop,${aws_instance.user-desktop.private_ip}
+
+EOF
+}
+
+resource "local_file" "ans-inv-csv" {
+  filename = "C:\\repos\\ownrepo\\myansiblelab\\excel-inv.csv"
   content  = <<EOF
 ad-root,${aws_instance.ad-root.private_ip}
 
